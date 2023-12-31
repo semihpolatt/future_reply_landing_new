383a818ead0a17aa7f04b4d58230a9523305e0d1
Semih Polat <72199891+semihpolatt@users.noreply.github.com>
2023-12-30 21:56:32 -0800

    fix errors

--- lib/home.dart
+++ lib/home.dart
@@ -13,155 +13,93 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Positioned(
-            top: 100,
-            left: 500,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GradientAnimationText(
-                  duration: const Duration(seconds: 2),
-                  text: const Text(
-                    "FutureReply",
-                    style:
-                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
-                  ),
-                  colors: [
-                    Colors.grey.shade500,
-                    Colors.white,
-                    //Colors.teal,
-                  ],
-                ),
-                GradientAnimationText(
-                  duration: const Duration(seconds: 3),
-                  text: const Text(
-                    "Extreme Fast Reply",
-                    style:
-                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.w100),
-                  ),
-                  colors: [
-                    Colors.grey.shade500,
-                    Colors.white,
-                    //Colors.teal,
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    for (String heroTag in [
-                      'images/whatsapp.png',
-                      'images/x.png',
-                      'images/gmail.png',
-                      /* 'images/linkedin.png',
-                      'images/imessage.png',
-                      'images/slack.png', */
-                    ])
-                      Row(
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              Get.to(
-                                  QueryPage(
-                                    heroTag: heroTag,
-                                  ),
-                                  // curve: Curves.ease,
-                                  transition: Transition.circularReveal,
-                                  duration: const Duration(seconds: 2));
-                              /*    Get.toNamed('/queryPage',
-                                  arguments: {'heroTag': heroTag}); */
-                            },
-                            child: Hero(
-                              tag: heroTag,
-                              child: badges.Badge(
-                                badgeContent: Countdown(
-                                  seconds: 40,
-                                  build: (BuildContext context, double time) =>
-                                      Text(
-                                    (time * 10).toString(),
-                                    style: const TextStyle(color: Colors.white),
-                                  ),
-                                  interval: const Duration(milliseconds: 100),
-                                  onFinished: () {},
-                                ),
-                                child: Container(
-                                  height: 100,
-                                  width: 100,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: AssetImage(heroTag))),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          )
-                        ],
-                      ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    for (String heroTag in [
-                      /* 'images/whatsapp.png',
-                      'images/x.png',
-                      'images/gmail.png', */
-                      'images/linkedin.png',
-                      'images/imessage.png',
-                      'images/slack.png',
-                    ])
-                      Row(
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              Get.to(
-                                  QueryPage(
-                                    heroTag: heroTag,
-                                  ),
-                                  transition: Transition.circularReveal,
-                                  duration: const Duration(seconds: 2));
-                            },
-                            child: Hero(
-                              tag: heroTag,
-                              child: badges.Badge(
-                                badgeContent: Countdown(
-                                  seconds: 40,
-                                  build: (BuildContext context, double time) =>
-                                      Text(
-                                    (time * 10).toString(),
-                                    style: const TextStyle(color: Colors.white),
-                                  ),
-                                  interval: const Duration(milliseconds: 100),
-                                  onFinished: () {},
-                                ),
-                                child: Container(
-                                  height: 100,
-                                  width: 100,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: AssetImage(heroTag))),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          )
-                        ],
-                      ),
-                  ],
-                ),
-              ],
+          const SizedBox(
+            height: 100,
+          ),
+          GradientAnimationText(
+            duration: const Duration(seconds: 2),
+            text: const Text(
+              "FutureReply",
+              style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
             ),
+            colors: [
+              Colors.grey.shade500,
+              Colors.white,
+              //Colors.teal,
+            ],
+          ),
+          GradientAnimationText(
+            duration: const Duration(seconds: 3),
+            text: const Text(
+              "Extreme Fast Reply",
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w100),
+            ),
+            colors: [
+              Colors.grey.shade500,
+              Colors.white,
+              //Colors.teal,
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (String heroTag in [
+                // 'images/whatsapp.png',
+                'images/x.png',
+                'images/gmail.png',
+                'images/linkedin.png',
+                'images/imessage.png',
+                'images/slack.png',
+              ])
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Get.to(
+                            QueryPage(
+                              heroTag: heroTag,
+                            ),
+                            // curve: Curves.ease,
+                            transition: Transition.circularReveal,
+                            duration: const Duration(seconds: 2));
+                        /*    Get.toNamed('/queryPage',
+                            arguments: {'heroTag': heroTag}); */
+                      },
+                      child: Hero(
+                        tag: heroTag,
+                        child: badges.Badge(
+                          badgeContent: Countdown(
+                            seconds: 40,
+                            build: (BuildContext context, double time) => Text(
+                              (time * 10).toString(),
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            interval: const Duration(milliseconds: 100),
+                            onFinished: () {},
+                          ),
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(heroTag))),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
           ),
         ],
       ),
